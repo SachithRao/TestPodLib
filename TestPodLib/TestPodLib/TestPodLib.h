e6224f764d6c1141a13495462be3427761f2a3f0
Sachith Rao <smohanra@teksystems.com>
2020-11-26 22:14:14 +0530

    Added TestPodLib

---
+++ TestPodLib/TestPodLib/TestPodLib.h
@@ -0,0 +1,18 @@
+//
+//  TestPodLib.h
+//  TestPodLib
+//
+//  Created by Sachith Mohan Rao on 26/11/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPodLib.
+FOUNDATION_EXPORT double TestPodLibVersionNumber;
+
+//! Project version string for TestPodLib.
+FOUNDATION_EXPORT const unsigned char TestPodLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodLib/PublicHeader.h>
+
+
---
+++ TestPodLib/TestPodLibExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestPodLibExamples
+//
+//  Created by Sachith Mohan Rao on 26/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
