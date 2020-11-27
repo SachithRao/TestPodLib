09ab3a656372f25f5fd0e8ac3bc3ba6e87d7cf58
Sachith Rao <smohanra@teksystems.com>
2020-11-27 11:27:14 +0530

    added TestPodLib implementation

---
+++ TestPodLib/TestPodLib/TestPodLib.swift
@@ -0,0 +1,22 @@
+//
+//  TestPodLib.swift
+//  TestPodLib
+//
+//  Created by Sachith Mohan Rao on 27/11/20.
+//
+
+import UIKit
+
+public final class TestPodLib {
+
+    let name = "TestPodLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- TestPodLib/TestPodLibTests/TestPodLibTests.swift
+++ TestPodLib/TestPodLibTests/TestPodLibTests.swift
@@ -10,24 +10,23 @@ import XCTest
 
 class TestPodLibTests: XCTestCase {
 
+    var testPodLib: TestPodLib!
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        testPodLib = TestPodLib()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testPodLib = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAdd() {
+        XCTAssertEqual(testPodLib.add(a: 2, b: 3), 5)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testSub() {
+        XCTAssertEqual(testPodLib.sub(a: 3, b: 2), 1)
     }
-
 }
